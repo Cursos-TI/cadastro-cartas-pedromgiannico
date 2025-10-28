e9d60ab679787d412cd657e12303b10c8d3ef962
Pedro <230078362+pedromgiannico@users.noreply.github.com>
2025-10-28 02:27:12 +0000

    AVENTUREIRO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,12 +8,14 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
     char estado;                          // Letra de 'A' a 'H'
-    char codcarta[4];                     // Ex: A01, B03
+    char codcarta[3];                     // Ex: A01, B03
     char nome[50];                        // Nome da cidade
     int pop;                              // Número de habitantes
     float area;                           // Área em km²
-    float pib;                            // PIB da cidade
+    float pib;                            // PIB da cidade em bilhões de reais
     int npt;                              // Número de pontos turísticos
+    float denpop;                         // Densidade populacional
+    float pibcap;                         // PIB per capita
 
   // Área para entrada de dados
 
@@ -32,20 +34,32 @@ int main() {
   printf("Digite a Área (em Km²): \n");
   scanf("%f", &area);
 
-  printf("Digite o PIB: \n");
+  printf("Digite o PIB (em bilhões de reais): \n");
   scanf("%f", &pib);
 
   printf("Digite o Número de Pontos Turísticos: \n");
   scanf("%d", &npt);
 
+  
   // Área para exibição dos dados da cidade
 
-  printf("Código da Carta: %c%s - Estado: %c\n", estado, codcarta, estado);
+  printf("Código da Carta: %c%s\n", estado, codcarta);
+  printf("Estado: %c\n", estado);
   printf("Cidade: %s\n", nome);
   printf("População: %d\n", pop);
-  printf("Área: %f\n", area);
-  printf("PIB: %f\n", pib);
-  printf("Pontos Turísticos: %d", npt);
+  printf("Área: %.2f km²\n", area);
+  printf("PIB: %.2f bilhões de reais\n", pib);
+  printf("Pontos Turísticos: %d\n", npt);
+
+  // densidade pop. - população / área
+
+  denpop = (float) pop / area;
+  printf("A densidade populacinal é: %.2f hab/km²\n", denpop);
+
+  // PIB per capita - PIB / população
+
+  pibcap = (float) (pib * 1000000000) / pop;
+    printf("O PIB per capita é: %.2f reais\n", pibcap);
 
 return 0;
 } 
